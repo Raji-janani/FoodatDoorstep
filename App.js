8e7c5579fc08ef57bd3c69d13c627e613026d68d
rajalakshmi <rajalakshmi3699@gmail.com>
2024-06-27 11:57:13 +0530

    React elements and components

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement("div", {id:"child"},
-        [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h1 tag")]
-    )
-    );
-    console.log(parent);
+
+
+const elem1 =(
+    <h2>this is element1</h2>
+);
+
+//React element inside another react element
+
+const elem2 = (
+    <div><span>{elem1}</span> this is element2</div>
+)
+
+
+const Title = () =>(
+<h1>This is from Title component</h1>
+
+);
+
+//React component inside another react element
+const elem3 =(
+   <div>this is elem3 and has title component text {Title()} and also <span><Title/></span></div>
+   
+)
+
+//React component inside another react component
+const Heading = () => (
+    <div id="container">
+        {elem1}
+        {elem2}
+        {Title()}
+        <Title/>
+        <h2>This is from heading component</h2>
+    </div>
+);
+    
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(elem3);
